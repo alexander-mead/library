aff5d9ec614391753bbd9eaf82d6d10a4e97e5a9
Alexander Mead <alexander.j.mead@googlemail.com>
2018-05-21 16:26:43 -0700

    added adaptive grid routines

--- fitting.f90
+++ fitting.f90
@@ -6,16 +6,145 @@ MODULE fitting
 
 CONTAINS
 
+  SUBROUTINE adapt_1(xmin,func,min,max,ref,ngrid,nref)
+
+    IMPLICIT NONE
+    REAL, INTENT(OUT) :: xmin
+    REAL, INTENT(IN) :: min, max, ref
+    INTEGER, INTENT(IN) :: ngrid, nref
+    REAL :: fom, fommin, range, x
+    REAL :: x1, x2
+    INTEGER :: i, j, imin
+
+    INTERFACE
+       REAL FUNCTION func(x)
+         REAL, INTENT(IN) :: x
+       END FUNCTION func
+    END INTERFACE
+
+    !xmin is output
+    !min/max give you the initial bounds for the search
+    !ref is the refinement level for each separate grid search
+    !n is the number of points per grid search
+    !m is the number of refinement levels
+
+    !Needs to be set to prevent warning
+    imin=1
+
+    !Needs to be set to prevent warnings
+    fommin=0.
+
+    x1=min
+    x2=max
+
+    DO j=1,nref
+
+       DO i=1,ngrid
+
+          x=x1+(x2-x1)*float(i-1)/float(ngrid-1)
+          fom=func(x)
+
+          IF(i==1 .OR. fom<fommin) THEN
+             fommin=fom
+             xmin=x
+             imin=i
+          END IF
+
+       END DO
+
+       IF(imin==1 .OR. imin==ngrid) STOP 'Error: best fit at edge of range'
+
+       IF(j .NE. nref) THEN
+          range=x2-x1
+          x1=xmin-range/ref
+          x2=xmin+range/ref
+       END IF
+
+    END DO
+
+  END SUBROUTINE adapt_1
+
+  SUBROUTINE adapt_3(xmin,func,min,max,ref,ngrid,nref)
+
+    IMPLICIT NONE
+    REAL, INTENT(OUT) :: xmin(3)
+    REAL, INTENT(IN) :: min(3), max(3), ref(3)
+    INTEGER, INTENT(IN) :: ngrid(3), nref
+    REAL, EXTERNAL :: func
+    REAL :: fom, fommin, range(3), x(3)
+    REAL :: x1(3), x2(3)
+    INTEGER :: i, j, k, l, imin, jmin, kmin
+
+    !xmin is output
+    !min/max give you the initial bounds for the search
+    !ref is the refinement level for each separate grid search
+    !n is the number of points per grid search
+    !m is the number of refinement levels
+
+    !Need to be set to prevent warnings
+    imin=1
+    jmin=1
+    kmin=1
+
+    !Needs to be set to prevent warnings
+    fommin=0.
+
+    x1=min
+    x2=max
+
+    DO l=1,nref
+
+       DO i=1,ngrid(1)
+
+          x(1)=x1(1)+(x2(1)-x1(1))*float(i-1)/float(ngrid(1)-1)
+
+          DO j=1,ngrid(2)
+
+             x(2)=x1(2)+(x2(2)-x1(2))*float(j-1)/float(ngrid(2)-1)
+            
+             DO k=1,ngrid(3)
+                
+                x(3)=x1(3)+(x2(3)-x1(3))*float(k-1)/float(ngrid(3)-1)
+
+                fom=func(x)
+                
+                IF((i==1 .AND. j==1 .AND. k==1) .OR. fom<fommin) THEN
+                   fommin=fom
+                   xmin=x
+                   imin=i
+                   jmin=j
+                   kmin=k
+                END IF
+
+             END DO
+             
+          END DO
+
+       END DO
+
+       IF(imin==1 .OR. imin==ngrid(1)) STOP 'Error: best fit x(1) at edge of range'
+       IF(jmin==1 .OR. jmin==ngrid(2)) STOP 'Error: best fit x(2) at edge of range'
+       IF(kmin==1 .OR. kmin==ngrid(3)) STOP 'Error: best fit x(3) at edge of range'
+
+       IF(l .NE. nref) THEN
+          range=x2-x1
+          x1=xmin-range/ref
+          x2=xmin+range/ref
+       END IF
+
+    END DO
+
+  END SUBROUTINE adapt_3
+
   SUBROUTINE grid_fit_1(Amin,Amax,Asteps,x,y)
 
+    !This fits a one parameter model
     IMPLICIT NONE
     REAL, INTENT(IN) :: Amin, Amax
     REAL, INTENT(IN) :: x(:), y(:)
     INTEGER, INTENT(IN) :: Asteps
     REAL :: ymod, fom, fombest, A, Abest
-    INTEGER :: i, j
-
-    !This fits a one parameter model
+    INTEGER :: i, j    
 
     fombest=10000000.
 
