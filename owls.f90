ce3f157bc6694c48d6f520d22008de5476731d92
Alexander Mead <alexander.j.mead@googlemail.com>
2018-06-08 10:57:22 -0700

    Removed own constants and made dependent on constants module; also made more explicit that it is using BAHAMAS thermal parameters

--- owls.f90
+++ owls.f90
@@ -2,18 +2,18 @@ MODULE owls
 
   IMPLICIT NONE
 
-  !Simulation parameters
+  !BAHAMAS simulation parameters
   REAL, PARAMETER :: fh=0.752 !Hydrogen mass fraction
   REAL, PARAMETER :: mu=0.61 !Mean molecular weight
-  REAL, PARAMETER :: Xe=1.17 !Electron fraction
-  REAL, PARAMETER :: Xi=1.08 !Ionisation fraction
+  REAL, PARAMETER :: Xe=1.17 !Electron fraction (electrons per hydrogen)
+  REAL, PARAMETER :: Xi=1.08 !Ion fraction (ionisation per hydrogen)
 
   !Physical constants
-  REAL, PARAMETER :: msun=1.989e30 !Solar mass in kg
-  REAL, PARAMETER :: mp=1.6726e-27 !Proton mass in kg
-  REAL, PARAMETER :: Mpc=3.0857e22 !Mpc in m
-  REAL, PARAMETER :: cm=0.01 !cm in m
-  REAL, PARAMETER :: eV=1.60218e-12 !eV in erg
+  !REAL, PARAMETER :: msun=1.989e30 !Solar mass in kg
+  !REAL, PARAMETER :: mp=1.6726e-27 !Proton mass in kg
+  !REAL, PARAMETER :: Mpc=3.0857e22 !Mpc in m
+  !REAL, PARAMETER :: cm=0.01 !cm in m
+  !REAL, PARAMETER :: eV=1.60218e-12 !eV in erg
   
 CONTAINS
 
@@ -71,6 +71,7 @@ CONTAINS
 
   SUBROUTINE read_mccarthy_gas(x,m,kT,nh,n,infile)
 
+    USE constants
     IMPLICIT NONE
     CHARACTER(len=*), INTENT(IN) :: infile
     REAL, ALLOCATABLE, INTENT(OUT) :: x(:,:), m(:), nh(:), kT(:)
@@ -78,14 +79,15 @@ CONTAINS
     INTEGER, INTENT(OUT) :: n
     
     REAL, PARAMETER :: mfac=1e10 !Convert mass to Solar masses
+    REAL, PARAMETER :: eV_erg=eV*1e7 !eV in ergs
 
     !Read in the binary file
-    WRITE(*,*) 'READ_MCCARTHY: Reading in binary file: ', TRIM(infile)
+    WRITE(*,*) 'READ_MCCARTHY_GAS: Reading in binary file: ', TRIM(infile)
     OPEN(7,file=infile,form='unformatted',access='stream',status='old')
     READ(7) n
     CLOSE(7)
-    WRITE(*,*) 'READ_MCCARTHY: Particle number:', n
-    WRITE(*,*) 'READ_MCCARTHY: Which is ~', NINT(n**(1./3.)), 'cubed.'
+    WRITE(*,*) 'READ_MCCARTHY_GAS: Particle number:', n
+    WRITE(*,*) 'READ_MCCARTHY_GAS: Which is ~', NINT(n**(1./3.)), 'cubed.'
 
     !Allocate arrays for quantities in the file
     ALLOCATE(x(3,n),m(n),ep(n),nh(n))
@@ -102,37 +104,46 @@ CONTAINS
     !Convert masses into Solar masses
     m=m*mfac
 
+    WRITE(*,*) 'READ_MCCARTHY_GAS: Calculating kT from electron pressure'
+    WRITE(*,*) 'READ_MCCARTHY_GAS: Using numbers appropriate for BAHAMAS'
+    WRITE(*,*) 'READ_MCCARTHY_GAS: YH:', fh
+    WRITE(*,*) 'READ_MCCARTHY_GAS: mu_H:', mu
+    WRITE(*,*) 'READ_MCCARTHY_GAS: Xe:', Xe
+    WRITE(*,*) 'READ_MCCARTHY_GAS: Xi:', Xi
+    
     !Convert the electron pressure [erg/cm^3] and hydrogen density [#/cm^3] into kT
     !Units of kT will be [erg]
+    !This is the temperature of gas particles (equal for all species)
     ALLOCATE(kT(n))
     kT=((Xe+Xi)/Xe)*(ep/nh)*mu*fh
 
     !Convert internal energy from erg to eV
-    kT=kT/eV
+    kT=kT/eV_erg
 
     !Deallocate the electron pressure array
     DEALLOCATE(ep)
 
     !Write information to the screen
-    WRITE(*,*) 'READ_MCCARTHY: Minimum particle mass [Msun/h]:', MINVAL(m)
-    WRITE(*,*) 'READ_MCCARTHY: Maximum particle mass [Msun/h]:', MAXVAL(m)
-    WRITE(*,*) 'READ_MCCARTHY: Minimum x coordinate [Mpc/h]:', MINVAL(x(1,:))
-    WRITE(*,*) 'READ_MCCARTHY: Minimum x coordinate [Mpc/h]:', MAXVAL(x(1,:))
-    WRITE(*,*) 'READ_MCCARTHY: Minimum y coordinate [Mpc/h]:', MINVAL(x(2,:))
-    WRITE(*,*) 'READ_MCCARTHY: Minimum y coordinate [Mpc/h]:', MAXVAL(x(2,:))
-    WRITE(*,*) 'READ_MCCARTHY: Minimum z coordinate [Mpc/h]:', MINVAL(x(3,:))
-    WRITE(*,*) 'READ_MCCARTHY: Minimum z coordinate [Mpc/h]:', MAXVAL(x(3,:))
-    WRITE(*,*) 'READ_MCCARTHY: Minimum internal energy [eV]:', MINVAL(kT)
-    WRITE(*,*) 'READ_MCCARTHY: Maximum internal energy [eV]:', MAXVAL(kT)
-    WRITE(*,*) 'READ_MCCARTHY: Minimum hydrogen number density [cm^-3]:', MINVAL(nh)
-    WRITE(*,*) 'READ_MCCARTHY: Maximum hydrogen number density [cm^-3]:', MAXVAL(nh)
-    WRITE(*,*) 'READ_MCCARTHY: Finished reading in file'
+    WRITE(*,*) 'READ_MCCARTHY_GAS: Minimum particle mass [Msun/h]:', MINVAL(m)
+    WRITE(*,*) 'READ_MCCARTHY_GAS: Maximum particle mass [Msun/h]:', MAXVAL(m)
+    WRITE(*,*) 'READ_MCCARTHY_GAS: Minimum x coordinate [Mpc/h]:', MINVAL(x(1,:))
+    WRITE(*,*) 'READ_MCCARTHY_GAS: Minimum x coordinate [Mpc/h]:', MAXVAL(x(1,:))
+    WRITE(*,*) 'READ_MCCARTHY_GAS: Minimum y coordinate [Mpc/h]:', MINVAL(x(2,:))
+    WRITE(*,*) 'READ_MCCARTHY_GAS: Minimum y coordinate [Mpc/h]:', MAXVAL(x(2,:))
+    WRITE(*,*) 'READ_MCCARTHY_GAS: Minimum z coordinate [Mpc/h]:', MINVAL(x(3,:))
+    WRITE(*,*) 'READ_MCCARTHY_GAS: Minimum z coordinate [Mpc/h]:', MAXVAL(x(3,:))
+    WRITE(*,*) 'READ_MCCARTHY_GAS: Minimum internal energy [eV]:', MINVAL(kT)
+    WRITE(*,*) 'READ_MCCARTHY_GAS: Maximum internal energy [eV]:', MAXVAL(kT)
+    WRITE(*,*) 'READ_MCCARTHY_GAS: Minimum hydrogen number density [cm^-3]:', MINVAL(nh)
+    WRITE(*,*) 'READ_MCCARTHY_GAS: Maximum hydrogen number density [cm^-3]:', MAXVAL(nh)
+    WRITE(*,*) 'READ_MCCARTHY_GAS: Finished reading in file'
     WRITE(*,*)
 
   END SUBROUTINE read_mccarthy_gas
 
   SUBROUTINE convert_kT_to_electron_pressure(kT,nh,mass,n,L,h,m)
 
+    USE constants
     IMPLICIT NONE
     REAL, INTENT(INOUT) :: kT(n)
     REAL, INTENT(IN) :: mass(n), nh(n), L, h
@@ -146,12 +157,18 @@ CONTAINS
     !Exclude gas that is sufficiently dense to not be ionised and be forming stars
     IF(apply_nh_cut) CALL exclude_nh(nh_cut,kT,nh,n)
 
+    WRITE(*,*) 'CONVERT_KT_TO_ELECTRON_PRESSURE: Converting kT to electron pressure'
+    WRITE(*,*) 'CONVERT_KT_TO_ELECTRON_PRESSURE: Using numbers appropriate for BAHAMAS'
+    WRITE(*,*) 'CONVERT_KT_TO_ELECTRON_PRESSURE: YH:', fh
+    WRITE(*,*) 'CONVERT_KT_TO_ELECTRON_PRESSURE: mu_H:', mu
+    WRITE(*,*) 'CONVERT_KT_TO_ELECTRON_PRESSURE: Xe:', Xe
+    WRITE(*,*) 'CONVERT_KT_TO_ELECTRON_PRESSURE: Xi:', Xi
+
     !Use double precision because all the constants are dreadful
     kT_dble=kT  
 
     !Convert to particle internal energy that needs to be mapped to grid [eV*Msun]
     kT_dble=kT_dble*(mass/mu)*Xe/(Xe+Xi)
-    !kT=kT*(mass/mu)*Xe/(Xe+Xi)
 
     !Cell volume in [(Mpc/h)^3]
     V=(L/REAL(m))**3
@@ -159,10 +176,10 @@ CONTAINS
     !Cell volume in [Mpc^3]
     V=V/h**3
 
-    !This is now pressure [eV/Mpc^3 * Msun]
+    !This is now electron pressure in [Msun*eV/Mpc^3]
     kT_dble=kT_dble/V
 
-    !Convert units of pressure [eV/cm^3]
+    !Convert units of electron pressure to [eV/cm^3]
     units=msun
     units=units/mp
     units=units/(Mpc/cm)
@@ -173,49 +190,10 @@ CONTAINS
     !Go back to single precision
     kT=REAL(kT_dble)
 
+    WRITE(*,*) 'CONVERT_KT_TO_ELECTRON_PRESSURE: Done'
+    WRITE(*,*)
+
   END SUBROUTINE convert_kT_to_electron_pressure
-  
-!!$  SUBROUTINE read_mccarthy_gas_old(x,m,ep,nh,n,infile)
-!!$
-!!$    IMPLICIT NONE
-!!$    CHARACTER(len=256), INTENT(IN) :: infile
-!!$    REAL, ALLOCATABLE, INTENT(OUT) :: x(:,:), m(:), ep(:), nh(:)
-!!$    INTEGER, INTENT(OUT) :: n
-!!$    REAL, PARAMETER :: mfac=1e10
-!!$
-!!$    WRITE(*,*) 'READ_MCCARTHY: Reading in binary file: ', TRIM(infile)
-!!$
-!!$    OPEN(7,file=infile,form='unformatted',access='stream',status='old')
-!!$    READ(7) n
-!!$    CLOSE(7)
-!!$
-!!$    WRITE(*,*) 'READ_MCCARTHY: Particle number:', n
-!!$    WRITE(*,*) 'READ_MCCARTHY: Which is ~', NINT(n**(1./3.)), 'cubed.'
-!!$
-!!$    ALLOCATE(x(3,n),m(n),ep(n),nh(n))
-!!$    
-!!$    !Need to read in 'n' again with stream access
-!!$    OPEN(7,file=infile,form='unformatted',access='stream',status='old')
-!!$    READ(7) n
-!!$    READ(7) m
-!!$    READ(7) x
-!!$    READ(7) ep
-!!$    READ(7) nh
-!!$    CLOSE(7)
-!!$
-!!$    m=m*mfac
-!!$
-!!$    WRITE(*,*) 'READ_MCCARTHY: Minimum and maximum mass [Msun/h]:', MINVAL(m), MAXVAL(m)
-!!$    WRITE(*,*) 'READ_MCCARTHY: Minimum and maximum x coordinate [Mpc/h]:', MINVAL(x(1,:)), MAXVAL(x(1,:))
-!!$    WRITE(*,*) 'READ_MCCARTHY: Minimum and maximum y coordinate [Mpc/h]:', MINVAL(x(2,:)), MAXVAL(x(2,:))
-!!$    WRITE(*,*) 'READ_MCCARTHY: Minimum and maximum z coordinate [Mpc/h]:', MINVAL(x(3,:)), MAXVAL(x(3,:))
-!!$    WRITE(*,*) 'READ_MCCARTHY: Minimum and maximum electron pressure [erg/cm^3]:', MINVAL(ep), MAXVAL(ep)
-!!$    WRITE(*,*) 'READ_MCCARTHY: Minimum and maximum hydrogen number density [cm^-3]:', MINVAL(nh), MAXVAL(nh)
-!!$    WRITE(*,*) 'READ_MCCARTHY: Finished reading in file'
-!!$
-!!$    WRITE(*,*)
-!!$
-!!$  END SUBROUTINE read_mccarthy_gas_old
 
   SUBROUTINE write_mccarthy(x,m,n,outfile)
 
