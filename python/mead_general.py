c509bb247ad0690b9cd6d4909dc0e5ff48daef40
Alexander Mead <alexander.j.mead@googlemail.com>
2019-02-19 13:06:40 -0800

    Added simple rectangular integration scheme

--- python/mead_general.py
+++ python/mead_general.py
@@ -2,14 +2,6 @@ import numpy as np
 from scipy import interpolate
 from scipy import integrate
 
-# From https://stackoverflow.com/questions/29346292/logarithmic-interpolation-in-python
-#def log_interp1d(xx, yy, kind='cubic'):
-#    logx = np.log10(xx)
-#    logy = np.log10(yy)
-#    lin_interp = interpolate.interp1d(logx, logy, kind=kind)
-#    log_interp = lambda zz: np.power(10.0, lin_interp(np.log10(zz)))
-#    return log_interp
-
 # My version of: https://stackoverflow.com/questions/29346292/logarithmic-interpolation-in-python
 # log10 replaced by log and other options for interp1d added, variables renamed
 def log_interp1d(x,y,\
@@ -30,18 +22,6 @@ def log_interp1d(x,y,\
                                       assume_sorted=assume_sorted)
     log_interp = lambda z: np.exp(lin_interp(np.log(z)))
     return log_interp
-
-## Logarithmically space a set of points
-## What about np.logspace??
-#def logspace(xmin,xmax,nx,\
-#             endpoint=True,\
-#             retstep=False,\
-#             dtype=None):
-#    return np.exp(np.linspace(np.log(xmin),np.log(xmax),nx,\
-#                              endpoint=endpoint,\
-#                              retstep=retstep,\
-#                              dtype=dtype))
-
 
 # A routine to integrate in log space. 
 # This may actually be pretty useless... not sure. Should do speed tests
@@ -72,3 +52,10 @@ def integrate_quad_log(func,a,b,\
                        maxp1=maxp1,\
                        limlst=limlst)
     return ans
+
+# A very simple rectangular integration that assumes equal sized bins
+def integrate_rectangular(fx,x):
+    dx = x[1]-x[0]
+    return sum(fx)*dx
+
+    
