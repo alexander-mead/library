a0eaa482436263b31dd570fa0cb2554f61b142e7
Alexander Mead <alexander.j.mead@googlemail.com>
2019-02-21 09:35:36 -0800

    2D probability draw function

--- python/mead_statistics.py
+++ python/mead_statistics.py
@@ -30,3 +30,56 @@ def variance(f,x1,x2,*args):
     m1 = moment(1,f,x1,x2,*args)
     m2 = moment(2,f,x1,x2,*args)
     return m2-m1**2
+
+# Draw random numbers from a 2D distribution
+# n - number of draws to make from f
+# f - f(x,y) to draw from
+# x1,x2 - limits on x axis
+# y1,y2 - limits on y axis
+# nx - number of points along x axis
+# ny - number of points along y axis
+def draw_from_2D(n,f,x1,x2,nx,y1,y2,ny):
+
+    # Pixel sizes in x and y
+    dx = (x2-x1)/np.real(nx)
+    dy = (y2-y1)/np.real(ny)
+
+    # Linearly spaced arrays of values corresponding to pixel centres
+    x = np.linspace(x1,x2,nx) 
+    y = np.linspace(y1,y2,ny)
+
+    # Make a grid of xy coordinate pairs
+    xy = np.array(np.meshgrid(x,y))
+    xy = xy.reshape(2,nx*ny) # Reshape the grid (2 here coresponds to 2 coordinates: x,y)
+    xy = np.transpose(xy).tolist() # Convert to a long list
+    
+    # Make array of function values corresponding to the xy coordinates
+    X,Y = np.meshgrid(x,y)
+    z = f(X,Y) # Array of function values
+    z = z.flatten() # Flatten array to create a long list of function values
+    z = z/sum(z) # Force normalisation
+
+    # Make a list of integers linking xy coordiantes to function values
+    i = list(range(z.size)) 
+
+    # Make the random choices with probabilties proportional to the function value
+    # The integer chosen by this can then be matched to the xy coordiantes
+    j = np.random.choice(i,n,replace=True,p=z) 
+    
+    # Now match the integers to the xy coordinates
+    xs = []
+    ys = []
+    for i in range(n):
+        xi,yi = xy[j[i]]
+        xs.append(xi)
+        ys.append(yi)
+
+    # Random numbers for inter-pixel displacement
+    dxs = np.random.uniform(-dx/2.,dx/2.,n) 
+    dys = np.random.uniform(-dy/2.,dy/2.,n)
+
+    # Uniform-random displacement within a pixel
+    xs = xs+dxs
+    ys = ys+dys
+        
+    return xs,ys
