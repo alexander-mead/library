f83134e8154ff8c59a8c6a16a8705af02b7b5846
Alexander Mead <alexander.j.mead@googlemail.com>
2019-11-28 22:40:12 +0100

    rewrite of 3D interpolation

--- src/interpolate.f90
+++ src/interpolate.f90
@@ -649,11 +649,132 @@ CONTAINS
 
    END FUNCTION find_2D
 
+   ! REAL FUNCTION find_3D(x, xin, y, yin, z, zin, fin, nx, ny, nz, iorder, ifind, iinterp)
+
+   !    ! A 3D interpolation routine to find value f(x,y,z) given a function evalated on arrays
+   !    ! The linear version implemented here is also know as 'trilinear interpolation'
+   !    ! TODO: Implement loops over coordinates to avoid repetition
+   !    IMPLICIT NONE
+   !    REAL, INTENT(IN) :: x
+   !    REAL, INTENT(IN) :: xin(nx)
+   !    REAL, INTENT(IN) :: y
+   !    REAL, INTENT(IN) :: yin(ny)
+   !    REAL, INTENT(IN) :: z
+   !    REAL, INTENT(IN) :: zin(nz)
+   !    REAL, INTENT(IN) :: fin(nx, ny, nz)
+   !    INTEGER, INTENT(IN) :: nx
+   !    INTEGER, INTENT(IN) :: ny
+   !    INTEGER, INTENT(IN) :: nz
+   !    INTEGER, INTENT(IN) :: iorder
+   !    INTEGER, INTENT(IN) :: ifind
+   !    INTEGER, INTENT(IN) :: iinterp
+   !    REAL :: x1, x2, y1, y2, z1, z2
+   !    REAL :: F(2, 2, 2)
+   !    REAL :: V(2, 2, 2)
+   !    INTEGER :: ix, ix1, ix2, iy, iy1, iy2, iz, iz1, iz2
+
+   !    ! If the value required is off the table edge then this will halt
+
+   !    ! iorder = 1 => linear interpolation
+
+   !    ! ifind = 1 => find x in xtab by crudely searching from x(1) to x(n)
+   !    ! ifind = 2 => find x in xtab quickly assuming the table is linearly spaced
+   !    ! ifind = 3 => find x in xtab using midpoint splitting (iterations=ceiling(log2(n)))
+
+   !    ! iinterp = 1 => Uses cubic polynomials for interpolation
+
+   !    IF (iinterp .NE. 1) STOP 'FIND_3D: No Lagrange polynomials for you, only regular polynomails, iinterp=1'
+
+   !    IF (xin(1) > xin(nx)) STOP 'FIND_3D: x array in wrong order'
+   !    IF (yin(1) > yin(ny)) STOP 'FIND_3D: y array in wrong order'
+   !    IF (zin(1) > zin(nz)) STOP 'FIND_3D: z array in wrong order'
+
+   !    IF (iorder == 1) THEN
+
+   !       IF (nx < 2) STOP 'FIND_3D: Not enough x points in your array for linear interpolation'
+   !       IF (ny < 2) STOP 'FIND_3D: Not enough y points in your array for linear interpolation'
+   !       IF (nz < 2) STOP 'FIND_3D: Not enough z points in your array for linear interpolation'
+
+   !       !! Get the x,y,z values !!
+
+   !       ! Get the integer coordinates in the x direction
+   !       ix = find_table_integer(x, xin, nx, ifind)
+   !       IF(ix==0) THEN
+   !          ix=1
+   !       ELSE IF(ix==nx) THEN
+   !          ix=nx-1
+   !       END IF
+   !       ix1 = ix
+   !       ix2 = ix+1
+
+   !       ! Get the x values
+   !       x1 = xin(ix1)
+   !       x2 = xin(ix2)
+
+   !       ! Get the integer coordinates in the y direction
+   !       iy = find_table_integer(y, yin, ny, ifind)
+   !       IF(iy==0) THEN
+   !          iy=1
+   !       ELSE IF(iy==ny) THEN
+   !          iy=ny-1
+   !       END IF
+   !       iy1 = iy
+   !       iy2 = iy+1
+
+   !       ! Get the y values
+   !       y1 = yin(iy1)
+   !       y2 = yin(iy2)
+
+   !       ! Get the integer coordinates in the z direction
+   !       iz = find_table_integer(z, zin, nz, ifind)
+   !       IF(iz==0) THEN
+   !          iz=1
+   !       ELSE IF(iz==nz) THEN
+   !          iz=nz-1
+   !       END IF
+   !       iz1 = iz
+   !       iz2 = iz+1
+
+   !       ! Get the z values
+   !       z1 = zin(iz1)
+   !       z2 = zin(iz2)
+
+   !       !! Function values !!
+
+   !       F(1, 1, 1) = fin(ix1, iy1, iz1)
+   !       F(1, 1, 2) = fin(ix1, iy1, iz2)
+   !       F(1, 2, 1) = fin(ix1, iy2, iz1)
+   !       F(1, 2, 2) = fin(ix1, iy2, iz2)
+   !       F(2, 1, 1) = fin(ix2, iy1, iz1)
+   !       F(2, 1, 2) = fin(ix2, iy1, iz2)
+   !       F(2, 2, 1) = fin(ix2, iy2, iz1)
+   !       F(2, 2, 2) = fin(ix2, iy2, iz2)
+
+   !       !! !!
+
+   !       V(1, 1, 1) = (x2-x)*(y2-y)*(z2-z)*F(1, 1, 1)
+   !       V(1, 1, 2) = (x2-x)*(y2-y)*(z-z1)*F(1, 1, 2)
+   !       V(1, 2, 1) = (x2-x)*(y-y1)*(z2-z)*F(1, 2, 1)
+   !       V(1, 2, 2) = (x2-x)*(y-y1)*(z-z1)*F(1, 2, 2)
+   !       V(2, 1, 1) = (x-x1)*(y2-y)*(z2-z)*F(2, 1, 1)
+   !       V(2, 1, 2) = (x-x1)*(y2-y)*(z-z1)*F(2, 1, 2)
+   !       V(2, 2, 1) = (x-x1)*(y-y1)*(z2-z)*F(2, 2, 1)
+   !       V(2, 2, 2) = (x-x1)*(y-y1)*(z-z1)*F(2, 2, 2)
+
+   !       find_3D = sum(V)/((x2-x1)*(y2-y1)*(z2-z1))
+
+   !    ELSE
+
+   !       STOP 'FIND_3D: order for interpolation not specified correctly, only linear implemented'
+
+   !    END IF
+
+   ! END FUNCTION find_3D
+
    REAL FUNCTION find_3D(x, xin, y, yin, z, zin, fin, nx, ny, nz, iorder, ifind, iinterp)
 
       ! A 3D interpolation routine to find value f(x,y,z) given a function evalated on arrays
       ! The linear version implemented here is also know as 'trilinear interpolation'
-      ! TODO: Implement loops over coordinates to avoid repetition
       IMPLICIT NONE
       REAL, INTENT(IN) :: x
       REAL, INTENT(IN) :: xin(nx)
@@ -668,10 +789,11 @@ CONTAINS
       INTEGER, INTENT(IN) :: iorder
       INTEGER, INTENT(IN) :: ifind
       INTEGER, INTENT(IN) :: iinterp
-      REAL :: x1, x2, y1, y2, z1, z2
+      REAL :: xx(3), x12(3, 2), Dx(3)
+      INTEGER :: ix(3), ix12(3, 2), nnx(3)
+      INTEGER :: i, j, k, ii, jj, kk, d, di
       REAL :: F(2, 2, 2)
       REAL :: V(2, 2, 2)
-      INTEGER :: ix, ix1, ix2, iy, iy1, iy2, iz, iz1, iz2
 
       ! If the value required is off the table edge then this will halt
 
@@ -691,77 +813,85 @@ CONTAINS
 
       IF (iorder == 1) THEN
 
-         IF (nx < 2) STOP 'FIND_3D: Not enough x points in your array for linear interpolation'
-         IF (ny < 2) STOP 'FIND_3D: Not enough y points in your array for linear interpolation'
-         IF (nz < 2) STOP 'FIND_3D: Not enough z points in your array for linear interpolation'
+         xx(1)=x
+         xx(2)=y
+         xx(3)=z
+
+         nnx(1)=nx
+         nnx(2)=ny
+         nnx(3)=nz
+
+         DO d = 1, 3
+            IF (nnx(d) < 2) STOP 'FIND_3D: Not enough points in your array for linear interpolation'
+         END DO
 
          !! Get the x,y,z values !!
 
-         ! Get the integer coordinates in the x direction
-         ix = find_table_integer(x, xin, nx, ifind)
-         IF(ix==0) THEN
-            ix=1
-         ELSE IF(ix==nx) THEN
-            ix=nx-1
-         END IF
-         ix1 = ix
-         ix2 = ix+1
+         ! Get the integer coordinates in each direction
+         DO d = 1, 3
 
-         ! Get the x values
-         x1 = xin(ix1)
-         x2 = xin(ix2)
+            ! Get the integer coordinates of the 'cube' corners that encompass the point in each dimensions
+            IF(d == 1) ix(1) = find_table_integer(x, xin, nx, ifind)
+            IF(d == 2) ix(2) = find_table_integer(y, yin, ny, ifind)
+            IF(d == 3) ix(3) = find_table_integer(z, zin, nz, ifind)
 
-         ! Get the integer coordinates in the y direction
-         iy = find_table_integer(y, yin, ny, ifind)
-         IF(iy==0) THEN
-            iy=1
-         ELSE IF(iy==ny) THEN
-            iy=ny-1
-         END IF
-         iy1 = iy
-         iy2 = iy+1
+            ! Correct for the case of these integers coordinates being at the edge of the box
+            IF(ix(d) == 0) THEN
+               ix(d) = 1
+            ELSE IF(ix(d) == nnx(d)) THEN
+               ix(d) = nnx(d)-1
+            END IF
+            ix12(d,1) = ix(d)
+            ix12(d,2) = ix(d)+1
 
-         ! Get the y values
-         y1 = yin(iy1)
-         y2 = yin(iy2)
+            ! Get the function value at the corners of the 'cube'
+            IF(d == 1) THEN
+               x12(d,1) = xin(ix12(d,1))
+               x12(d,2) = xin(ix12(d,2))
+            ELSE IF(d == 2) THEN
+               x12(d,1) = yin(ix12(d,1))
+               x12(d,2) = yin(ix12(d,2))
+            ELSE IF(d == 3) THEN
+               x12(d,1) = zin(ix12(d,1))
+               x12(d,2) = zin(ix12(d,2))
+            END IF
 
-         ! Get the integer coordinates in the z direction
-         iz = find_table_integer(z, zin, nz, ifind)
-         IF(iz==0) THEN
-            iz=1
-         ELSE IF(iz==nz) THEN
-            iz=nz-1
-         END IF
-         iz1 = iz
-         iz2 = iz+1
+         END DO 
 
-         ! Get the z values
-         z1 = zin(iz1)
-         z2 = zin(iz2)
+         ! Do the interpolation
+         DO k = 1, 2
+            DO j = 1, 2
+               DO i = 1, 2
 
-         !! Function values !!
+                  ! Evaluate the function at the cube corners
+                  F(i, j, k) = fin(ix12(1,i), ix12(2,j), ix12(3,k))
 
-         F(1, 1, 1) = fin(ix1, iy1, iz1)
-         F(1, 1, 2) = fin(ix1, iy1, iz2)
-         F(1, 2, 1) = fin(ix1, iy2, iz1)
-         F(1, 2, 2) = fin(ix1, iy2, iz2)
-         F(2, 1, 1) = fin(ix2, iy1, iz1)
-         F(2, 1, 2) = fin(ix2, iy1, iz2)
-         F(2, 2, 1) = fin(ix2, iy2, iz1)
-         F(2, 2, 2) = fin(ix2, iy2, iz2)
+                  ! Calculate the volume of the solid in the opposite corner
+                  ii = 3-i ! Swaps 1 <--> 2 (e.g., ii is 2 if i is 1 and visa versa)
+                  jj = 3-j ! Swaps 1 <--> 2
+                  kk = 3-k ! Swaps 1 <--> 2
+                  DO d = 1, 3
+                     IF(d==1) di=ii
+                     IF(d==2) di=jj
+                     IF(d==3) di=kk
+                     Dx(d) = (x12(d,di)-xx(d))*(2*di-3) ! Last part gets the sign right (x2-x) or (x-x1), not negatives (x-x2)!!
+                  END DO
 
-         !! !!
+                  ! Weight the corner function values by their corresponding weights
+                  V(i, j, k) = F(i, j, k)
+                  DO d = 1, 3
+                     V(i, j, k)=V(i, j, k)*Dx(d)
+                  END DO
 
-         V(1, 1, 1) = (x2-x)*(y2-y)*(z2-z)*F(1, 1, 1)
-         V(1, 2, 1) = (x2-x)*(y-y1)*(z2-z)*F(1, 2, 1)
-         V(2, 1, 1) = (x-x1)*(y2-y)*(z2-z)*F(2, 1, 1)
-         V(2, 2, 1) = (x-x1)*(y-y1)*(z2-z)*F(2, 2, 1)
-         V(1, 1, 2) = (x2-x)*(y2-y)*(z-z1)*F(1, 1, 2)
-         V(1, 2, 2) = (x2-x)*(y-y1)*(z-z1)*F(1, 2, 2)
-         V(2, 1, 2) = (x-x1)*(y2-y)*(z-z1)*F(2, 1, 2)
-         V(2, 2, 2) = (x-x1)*(y-y1)*(z-z1)*F(2, 2, 2)
+               END DO
+            END DO
+         END DO
 
-         find_3D = sum(V)/((x2-x1)*(y2-y1)*(z2-z1))
+         ! Finally normalise the result by dividing by the cube volume
+         find_3D = sum(V)
+         DO d = 1,3
+            find_3D = find_3D/(x12(d,2)-x12(d,1))
+         END DO
 
       ELSE
 
