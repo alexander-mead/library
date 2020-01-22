451041dc775b723e5cfe0b8122802f7ae589b742
Alexander Mead <alexander.j.mead@googlemail.com>
2020-01-22 10:29:05 +0100

    cleaned up a bit

--- src/string_operations.f90
+++ src/string_operations.f90
@@ -7,6 +7,7 @@ MODULE string_operations
    PUBLIC :: number_file
    PUBLIC :: number_file2
    PUBLIC :: number_file_zeroes
+   PUBLIC :: integer_to_string
 
 CONTAINS
 
@@ -22,6 +23,26 @@ CONTAINS
 !!$
 !!$  end subroutine str2int
 
+   CHARACTER(len=8) FUNCTION integer_to_string(i)
+
+      IMPLICIT NONE
+      INTEGER, INTENT(IN) :: i
+      CHARACTER(len=8) :: fmt   
+
+      IF (i >= 0 .AND. i < 10) THEN
+         fmt='(I1)'
+      ELSE IF (i >= 10 .OR. i < 100) THEN
+         fmt='(I2)'
+      ELSE IF (i >= 100 .OR. i < 1000) THEN
+         fmt='(I3)'
+      ELSE
+         STOP 'INTEGER_TO_STRING: Error, your integer is not supported'
+      END IF
+
+      WRITE (integer_to_string, fmt=fmt) i
+
+   END FUNCTION integer_to_string
+
    FUNCTION number_file(fbase, i, fext)
 
       IMPLICIT NONE
@@ -30,16 +51,7 @@ CONTAINS
       INTEGER, INTENT(IN) :: i
       CHARACTER(len=8) num
 
-      IF (i < 0) STOP 'NUMBER_FILE: Error: cannot write negative number file names'
-
-      IF (i < 10) THEN
-         WRITE (num, fmt='(I1)') i
-      ELSE IF (i < 100) THEN
-         WRITE (num, fmt='(I2)') i
-      ELSE IF (i < 1000) THEN
-         WRITE (num, fmt='(I3)') i
-      END IF
-
+      num = integer_to_string(i)
       number_file = trim(fbase)//trim(num)//trim(fext)
 
    END FUNCTION number_file
@@ -52,22 +64,8 @@ CONTAINS
       INTEGER, INTENT(IN) :: i1, i2
       CHARACTER(len=8) :: num1, num2
 
-      IF (i1 < 10) THEN
-         WRITE (num1, fmt='(I1)') i1
-      ELSE IF (i1 < 100) THEN
-         WRITE (num1, fmt='(I2)') i1
-      ELSE IF (i1 < 1000) THEN
-         WRITE (num1, fmt='(I3)') i1
-      END IF
-
-      IF (i2 < 10) THEN
-         WRITE (num2, fmt='(I1)') i2
-      ELSE IF (i2 < 100) THEN
-         WRITE (num2, fmt='(I2)') i2
-      ELSE IF (i2 < 1000) THEN
-         WRITE (num2, fmt='(I3)') i2
-      END IF
-
+      num1 = integer_to_string(i1)
+      num2 = integer_to_string(i2)
       number_file2 = trim(fbase)//trim(num1)//trim(mid)//trim(num2)//trim(fext)
 
    END FUNCTION number_file2
@@ -84,39 +82,29 @@ CONTAINS
       CHARACTER(len=2) :: num2
       CHARACTER(len=1) :: num1
       INTEGER, INTENT(IN) :: i
-      INTEGER :: maxnum
       INTEGER, INTENT(IN) :: num
-
-      maxnum = 4
+      INTEGER, PARAMETER :: maxnum = 4
 
       IF (i < 0) STOP 'NUMBER_FILE_ZEROES: Error: cannot write negative number file names'
 
       IF (num > maxnum) STOP 'NUMBER_FILE_ZEROES: Error: need to add extra number capacity'
 
       IF (num == 1) THEN
-
          IF (i >= 10) STOP 'NUMBER_FILE_ZEROES: Error: i is too large a number for space (i>num)'
          WRITE (num1, fmt='(I0.1)') i
          number_file_zeroes = trim(fbase)//num1//trim(fext)
-
       ELSE IF (num == 2) THEN
-
          IF (i >= 100) STOP 'NUMBER_FILE_ZEROES: Error: i is too large a number for space (i>num)'
          WRITE (num2, fmt='(I0.2)') i
          number_file_zeroes = trim(fbase)//num2//trim(fext)
-
       ELSE IF (num == 3) THEN
-
          IF (i >= 1000) STOP 'NUMBER_FILE_ZEROES: Error: i is too large a number for space (i>num)'
          WRITE (num3, fmt='(I0.3)') i
          number_file_zeroes = trim(fbase)//num3//trim(fext)
-
       ELSE IF (num == 4) THEN
-
          IF (i >= 10000) STOP 'NUMBER_FILE_ZEROES: Error: i is too large a number for space (i>num)'
          WRITE (num4, fmt='(I0.4)') i
          number_file_zeroes = trim(fbase)//trim(num4)//trim(fext)
-
       END IF
 
    END FUNCTION number_file_zeroes
