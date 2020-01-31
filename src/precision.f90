449305ce695f36f56fc28d2fd857cae51b180f02
Alexander Mead <alexander.j.mead@googlemail.com>
2020-01-31 19:42:22 +0100

    updated precision

--- src/precision.f90
+++ src/precision.f90
@@ -5,9 +5,14 @@ MODULE precision
    PRIVATE
 
    PUBLIC :: sp
-   PUBLIC :: dl
+   PUBLIC :: dp
+   PUBLIC :: qp
 
-   INTEGER, PARAMETER :: sp = KIND(1.0)
-   INTEGER, PARAMETER :: dl = KIND(1.d0)
+   !INTEGER, PARAMETER :: sp = KIND(1.0)
+   !INTEGER, PARAMETER :: dp = KIND(1.d0)
+   ! Taken from http://fortranwiki.org/fortran/show/Real+precision
+   INTEGER, PARAMETER :: sp = selected_real_kind(6, 37)    ! Single precision
+   INTEGER, PARAMETER :: dp = selected_real_kind(15, 307)  ! Double precision
+   INTEGER, PARAMETER :: qp = selected_real_kind(33, 4931) ! Quadruple precision
 
 END MODULE precision
