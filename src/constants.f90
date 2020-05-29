79e442153faff7ed8909e15f3cfd80f31c9861c9
Alexander Mead <alexander.j.mead@googlemail.com>
2020-05-29 20:52:15 +0100

    Added precision

--- src/constants.f90
+++ src/constants.f90
@@ -1,5 +1,6 @@
 MODULE constants
 
+   USE precision
    IMPLICIT NONE
 
    PUBLIC
@@ -7,31 +8,31 @@ MODULE constants
    !!
 
    ! Mathematical constants
-   REAL, PARAMETER :: pi = 3.14159265359 ! pi
-   REAL, PARAMETER :: em = 0.5772156649  ! Euler–Mascheroni
-   REAL, PARAMETER :: zero = 0.          ! zero
-   REAL, PARAMETER :: one = 1.           ! one
-   REAL, PARAMETER :: degrees = 360.     ! Degrees in a circle
+   REAL, PARAMETER :: pi = 3.14159265359_dp ! pi
+   REAL, PARAMETER :: em = 0.5772156649_dp  ! Euler–Mascheroni
+   REAL, PARAMETER :: zero = 0._dp          ! zero
+   REAL, PARAMETER :: one = 1._dp           ! one
+   REAL, PARAMETER :: degrees = 360._dp     ! Degrees in a circle
 
    ! Mathematical derived constants
-   REAL, PARAMETER :: twopi = 2.*pi           ! 2pi or tau ~6.283
+   REAL, PARAMETER :: twopi = 2._dp*pi        ! 2pi or tau ~6.283
    REAL, PARAMETER :: rad2deg = degrees/twopi ! radians-to-degrees conversion
-   REAL, PARAMETER :: deg2rad = 1./rad2deg    ! degress-to-radians conversion
+   REAL, PARAMETER :: deg2rad = 1._dp/rad2deg ! degress-to-radians conversion
 
    !!
 
    !!
 
    ! Physical constants
-   REAL, PARAMETER :: kB = 1.38064852e-23        ! Boltzmann constant [m^2 kg s^-2 K^-1]
-   REAL, PARAMETER :: mp = 1.6726219e-27         ! proton mass [kg]
-   REAL, PARAMETER :: me = 9.10938356e-31        ! electron mass [kg]
-   REAL, PARAMETER :: bigG = 6.67408e-11         ! Gravitational constant [kg^-1 m^3 s^-2]
-   REAL, PARAMETER :: eV = 1.60218e-19           ! electronvolt [kg m^2 s^-2]
-   REAL, PARAMETER :: SBconst = 5.670367e-8      ! Steffan-Boltzmann constant [kg s^-3 K^-4]
-   REAL, PARAMETER :: c_light = 2.99792458e8     ! speed of light [m/s]
-   REAL, PARAMETER :: sigma_T = 6.6524587158e-29 ! Thompson-scatter cross section [m^2]
-   REAL, PARAMETER :: h_Planck = 6.62607004e-34  ! Planck constant [kg m^2/s]
+   REAL, PARAMETER :: kB = 1.38064852e-23_dp        ! Boltzmann constant [m^2 kg s^-2 K^-1]
+   REAL, PARAMETER :: mp = 1.6726219e-27_dp         ! proton mass [kg]
+   REAL, PARAMETER :: me = 9.10938356e-31_dp        ! electron mass [kg]
+   REAL, PARAMETER :: bigG = 6.67408e-11_dp         ! Gravitational constant [kg^-1 m^3 s^-2]
+   REAL, PARAMETER :: eV = 1.60218e-19_dp           ! electronvolt [kg m^2 s^-2]
+   REAL, PARAMETER :: SBconst = 5.670367e-8_dp      ! Steffan-Boltzmann constant [kg s^-3 K^-4]
+   REAL, PARAMETER :: c_light = 2.99792458e8_dp     ! speed of light [m/s]
+   REAL, PARAMETER :: sigma_T = 6.6524587158e-29_dp ! Thompson-scatter cross section [m^2]
+   REAL, PARAMETER :: h_Planck = 6.62607004e-34_dp  ! Planck constant [kg m^2/s]
 
    ! Derived physical constants
    REAL, PARAMETER :: mp_eV = mp*(c_light)**2/eV ! Proton rest mass in eV [~938 MeV]
@@ -42,13 +43,13 @@ MODULE constants
    !!
 
    ! Astronomy constants
-   REAL, PARAMETER :: au = 149597870700.               ! Astronomical unit [m] (https://en.wikipedia.org/wiki/Astronomical_unit)
-   REAL, PARAMETER :: H0_cos = 100.                    ! Hubble parameter [km s^-1 (Mpc/h)^-1]
-   REAL, PARAMETER :: Msun = 1.98847e30                ! Solar mass [kg] (https://en.wikipedia.org/wiki/Solar_mass)
-   REAL, PARAMETER :: Jansky = 1e-26                   ! [W m^-2 Hz^-1 / Jy]
+   REAL, PARAMETER :: au = 149597870700._dp  ! Astronomical unit [m] (https://en.wikipedia.org/wiki/Astronomical_unit)
+   REAL, PARAMETER :: H0_cos = 100._dp       ! Hubble parameter [km s^-1 (Mpc/h)^-1]
+   REAL, PARAMETER :: Msun = 1.98847e30_dp   ! Solar mass [kg] (https://en.wikipedia.org/wiki/Solar_mass)
+   REAL, PARAMETER :: Jansky = 1e-26_dp      ! [W m^-2 Hz^-1 / Jy]
 
    ! Derived astronomy constants
-   REAL, PARAMETER :: parsec = 60.*60.*180*au/pi                           ! Parsec [m] ~3.086e16
+   REAL, PARAMETER :: parsec = 60.*60.*180.*au/pi                          ! Parsec [m] ~3.086e16
    REAL, PARAMETER :: kpc = parsec*1e3                                     ! kpc [m]
    REAL, PARAMETER :: Mpc = kpc*1e3                                        ! Mpc [m]
    REAL, PARAMETER :: Gpc = Mpc*1e3                                        ! Gpc [m]
@@ -63,8 +64,8 @@ MODULE constants
    REAL, PARAMETER :: Dv0 = 18.*pi**2                                      ! Einsten-de Sitter virialised collapse threshold ~178
 
    ! TODO: Relate these to fundamental constants
-   REAL, PARAMETER :: neutrino_constant = 94.1         ! Critical mass for neutrino density to close Universe [eV] (or is it 93.03 eV, or 93.14 eV?; https://arxiv.org/pdf/1812.02102.pdf)
-   REAL, PARAMETER :: neff_constant = (7./8.)*(4./11.)**(4./3.)
+   REAL, PARAMETER :: neutrino_constant = 94.1_dp               ! Critical mass for neutrino density to close Universe [eV] (or is it 93.03 eV, or 93.14 eV?; https://arxiv.org/pdf/1812.02102.pdf)
+   REAL, PARAMETER :: neff_constant = (7./8.)*(4./11.)**(4./3.) ! Constant that appears in temperature formulae with neutrinos
 
    !!
 
