b8c8496329e3ca14a768d69aaa7e2e9d79ae7eb5
Alexander Mead <alexander.j.mead@googlemail.com>
2020-07-29 12:58:45 +0100

    updated demo

--- demos/statistics_demo/statistics_demo.f90
+++ demos/statistics_demo/statistics_demo.f90
@@ -1,103 +1,106 @@
 PROGRAM statistics_test
 
-  USE statistics
-  USE table_integer
-  USE random_numbers
-  USE array_operations
+   USE statistics
+   USE table_integer
+   USE random_numbers
+   USE array_operations
 
-  IMPLICIT NONE
-  REAL, ALLOCATABLE :: A(:), bins(:)
-  INTEGER, ALLOCATABLE :: hist(:)
-  CHARACTER(len=256) :: outfile
-  REAL :: min, max, avg, std, x, dx, mu, sigma2
-  INTEGER :: i
-  INTEGER :: itest
+   IMPLICIT NONE
+   REAL, ALLOCATABLE :: A(:), bins(:)
+   INTEGER, ALLOCATABLE :: hist(:)
+   CHARACTER(len=256) :: outfile
+   REAL :: min, max, avg, std, x, dx, mu, sigma2
+   INTEGER :: i
+   INTEGER :: itest
 
-  REAL, PARAMETER :: x0=0.
-  REAL, PARAMETER :: sigma=1.
-  REAL, PARAMETER :: dmin=-5.
-  REAL, PARAMETER :: dmax=5.
-  INTEGER, PARAMETER :: nh=200
-  INTEGER, PARAMETER :: m=1000000
-  INTEGER, PARAMETER :: iseed=1
-  INTEGER, PARAMETER :: n=1000000
+   REAL, PARAMETER :: x0 = 0.
+   REAL, PARAMETER :: sigma = 1.
+   REAL, PARAMETER :: dmin = -5.
+   REAL, PARAMETER :: dmax = 5.
+   INTEGER, PARAMETER :: nh = 200
+   INTEGER, PARAMETER :: m = 1000000
+   INTEGER, PARAMETER :: iseed = 0
+   INTEGER, PARAMETER :: n = 1000000
 
-  WRITE(*,*)
-  WRITE(*,*) 'STATISTICS_TEST: Select test'
-  WRITE(*,*) '1 - Histogram'
-  WRITE(*,*) '2 - Mean and variance'
-  READ(*,*) itest
-  WRITE(*,*)
+   WRITE (*, *)
+   WRITE (*, *) 'STATISTICS_TEST: Select test'
+   WRITE (*, *) '1 - Histogram'
+   WRITE (*, *) '2 - Mean and variance'
+   READ (*, *) itest
+   WRITE (*, *)
 
-  !Seed the random number generator
-  CALL RNG_set(iseed)
+   !Seed the random number generator
+   CALL random_generator_seed(iseed)
 
-  IF(itest==1) THEN
+   IF (itest == 1) THEN
 
-     WRITE(*,*)
-     WRITE(*,*) 'STATISTICS_TEST: Welcome'
-     WRITE(*,*) 'STATISTICS_TEST: Calculating various one-point statistics'
+      WRITE (*, *)
+      WRITE (*, *) 'STATISTICS_TEST: Welcome'
+      WRITE (*, *) 'STATISTICS_TEST: Calculating various one-point statistics'
 
-     !Allocate the 'data' array
-     ALLOCATE(A(m))
+      !Allocate the 'data' array
+      ALLOCATE (A(m))
 
-     !Fill the 'data' array
-     WRITE(*,*) 'STATISTICS_TEST: Creating Gaussian random numbers'
-     WRITE(*,*) 'STATISTICS_TEST: Mean value:', x0
-     WRITE(*,*) 'STATISTICS_TEST: Standard deviation:', sigma
-     DO i=1,m
-        A(i)=random_Gaussian(x0,sigma)
-     END DO
+      !Fill the 'data' array
+      WRITE (*, *) 'STATISTICS_TEST: Creating Gaussian random numbers'
+      WRITE (*, *) 'STATISTICS_TEST: Mean value:', x0
+      WRITE (*, *) 'STATISTICS_TEST: Standard deviation:', sigma
+      DO i = 1, m
+         A(i) = random_Gaussian(x0, sigma)
+      END DO
 
-     !Calculate and write out some useful statistics
-     min=MINVAL(A)
-     max=MAXVAL(A)
-     avg=mean(A,m)
-     std=sqrt(variance(A,m))
-     WRITE(*,*) 'STATISTICS_TEST: Number of points:', m
-     WRITE(*,*) 'STATISTICS_TEST: Minimum value:', REAL(min)
-     WRITE(*,*) 'STATISTICS_TEST: Maximum value:', REAL(max)
-     WRITE(*,*) 'STATISTICS_TEST: Mean value:', REAL(avg)
-     WRITE(*,*) 'STATISTICS_TEST: Standard deviation:', REAL(std)
-     WRITE(*,*)
+      !Calculate and write out some useful statistics
+      min = MINVAL(A)
+      max = MAXVAL(A)
+      avg = mean(A)
+      std = sqrt(variance(A))
+      WRITE (*, *) 'STATISTICS_TEST: Number of points:', m
+      WRITE (*, *) 'STATISTICS_TEST: Minimum value:', REAL(min)
+      WRITE (*, *) 'STATISTICS_TEST: Maximum value:', REAL(max)
+      WRITE (*, *) 'STATISTICS_TEST: Mean value:', REAL(avg)
+      WRITE (*, *) 'STATISTICS_TEST: Standard deviation:', REAL(std)
+      WRITE (*, *)
 
-     !Create the one-point PDF
-     CALL histogram(dmin,dmax,bins,hist,nh,A,m)
+      !Create the one-point PDF
+      CALL histogram(dmin, dmax, bins, hist, nh, A)
 
-     !Writes out the normalised histogram to a file
-     outfile='histogram.dat'
-     WRITE(*,*) 'STATISTICS_TEST: Writing data'
-     OPEN(7,file=outfile)
-     DO i=1,nh
-        x=(bins(i)+bins(i+1))/2.
-        dx=bins(i+1)-bins(i)
-        WRITE(7,*) x, hist(i)/(REAL(m)*dx)
-     END DO
-     CLOSE(7)
+      !Writes out the normalised histogram to a file
+      outfile = 'histogram.dat'
+      WRITE (*, *) 'STATISTICS_TEST: Writing data'
+      OPEN (7, file=outfile)
+      DO i = 1, nh
+         x = (bins(i)+bins(i+1))/2.
+         dx = bins(i+1)-bins(i)
+         WRITE (7, *) x, hist(i)/(REAL(m)*dx)
+      END DO
+      CLOSE (7)
 
-     WRITE(*,*) 'STATISTICS_TEST: Done'
-     WRITE(*,*)
+      WRITE (*, *) 'STATISTICS_TEST: Done'
+      WRITE (*, *)
 
-  ELSE IF(itest==2) THEN
+   ELSE IF (itest == 2) THEN
 
-     WRITE(*,*) 'STATISTICS_TEST: Generating n uniform random numbers, n:', n
-     ALLOCATE(a(n))
-     DO i=1,n
-        a(i)=random_uniform(0.,1.)
-     END DO
+      WRITE (*, *) 'STATISTICS_TEST: Generating n uniform random numbers, n:', n
+      ALLOCATE (a(n))
+      DO i = 1, n
+         a(i) = random_uniform(0., 1.)
+      END DO
 
-     !WRITE(*,*) 'Data:', a
-     mu=mean(a,n)
-     sigma2=variance(a,n)
-     WRITE(*,*) 'STATISTICS_TEST: Mean:', mu
-     WRITE(*,*) 'STATISTICS_TEST: Variance:', sigma2
-     WRITE(*,*) 'STATISTICS_TEST: Standard deviation:', sqrt(sigma2)
-     WRITE(*,*)
+      !WRITE(*,*) 'Data:', a
+      mu = mean(a)
+      sigma2 = variance(a)
+      WRITE (*, *) 'STATISTICS_TEST: Expected mean:', 0.5
+      WRITE (*, *) 'STATISTICS_TEST: Expected variance:', 1./12.
+      WRITE (*, *) 'STATISTICS_TEST: Expected standard deviation', sqrt(1./12.)
+      WRITE (*, *) 'STATISTICS_TEST: Mean:', mu
+      WRITE (*, *) 'STATISTICS_TEST: Variance:', sigma2
+      WRITE (*, *) 'STATISTICS_TEST: Standard deviation:', sqrt(sigma2)
+      WRITE (*, *)
 
-  ELSE
+   ELSE
 
-     STOP 'STATISTICS_TEST: Error, itest not specified correctly'
+      STOP 'STATISTICS_TEST: Error, itest not specified correctly'
 
-  END IF
+   END IF
 
 END PROGRAM statistics_test
