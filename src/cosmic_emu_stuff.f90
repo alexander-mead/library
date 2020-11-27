dadf26acfb9ad3cb33331f277fb4fcd4a0fe44cd
Alexander Mead <alexander.j.mead@googlemail.com>
2020-11-27 16:17:22 +0000

    tidy; more stuff to header

--- src/cosmic_emu_stuff.f90
+++ src/cosmic_emu_stuff.f90
@@ -30,14 +30,25 @@ MODULE cosmic_emu_stuff
    !REAL, PARAMETER :: kmax_rebin_CosmicEmu = 2.7 ! This is the maximum
    !REAL, PARAMETER :: kmax_rebin_CosmicEmu = 0.2 ! Perturbation-theory studies
    INTEGER, PARAMETER :: nk_rebin_CosmicEmu = 128
+   CHARACTER(len=256), PARAMETER :: params_CosmicEmu = 'emu_params.txt'
+   CHARACTER(len=256), PARAMETER :: output_CosmicEmu = 'emu_power.dat'
+   CHARACTER(len=256), PARAMETER :: exe_CosmicEmu = '/Users/Mead/Physics/CosmicEmu/emu.exe'
+   INTEGER, PARAMETER :: nh_CosmicEmu = 10  ! Length of header
+   INTEGER, PARAMETER :: hl_CosmicEmu = 8   ! Line that h in on
 
    REAL, PARAMETER :: kmin_rebin_FrankenEmu = 1e-2
    REAL, PARAMETER :: kmax_rebin_FrankenEmu = 10.
    INTEGER, PARAMETER :: nk_rebin_FrankenEmu = 128
+   CHARACTER(len=256), PARAMETER :: params_FrankenEmu = 'emu_params.txt'
+   CHARACTER(len=256), PARAMETER :: output_FrankenEmu = 'emu_power.dat'
+   CHARACTER(len=256), PARAMETER :: exe_FrankenEmu = '/Users/Mead/Physics/FrankenEmu/emu.exe'
 
    REAL, PARAMETER :: kmin_rebin_MiraTitan = 1e-2
    REAL, PARAMETER :: kmax_rebin_MiraTitan = 7.
    INTEGER, PARAMETER :: nk_rebin_MiraTitan = 128
+   CHARACTER(len=256), PARAMETER :: params_MiraTitan = 'xstar.dat'
+   CHARACTER(len=256), PARAMETER :: output_MiraTitan = 'EMU0.txt'
+   CHARACTER(len=256), PARAMETER :: exe_MiraTitan = '/Users/Mead/Physics/MiraTitan/P_tot/emu.exe'
 
    INTEGER, PARAMETER :: iorder_rebin = 3
    INTEGER, PARAMETER :: ifind_rebin = ifind_split
@@ -118,27 +129,30 @@ CONTAINS
       INTEGER :: i
       CHARACTER(len=256) :: crap
       REAL :: h
-      INTEGER, PARAMETER :: nh = 10  ! Length of header
-      INTEGER, PARAMETER :: h_li = 8 ! Line that h in on
-      CHARACTER(len=256), PARAMETER :: params = 'emu_params.txt'
-      CHARACTER(len=256), PARAMETER :: output = 'emu_power.dat'
-      CHARACTER(len=256), PARAMETER :: exe = '/Users/Mead/Physics/CosmicEmu/emu.exe'
+      INTEGER, PARAMETER :: nh = nh_CosmicEmu ! Length of header
+      INTEGER, PARAMETER :: hl = hl_CosmicEmu ! Line that h in on
+      CHARACTER(len=256), PARAMETER :: params = params_CosmicEmu
+      CHARACTER(len=256), PARAMETER :: output = output_CosmicEmu
+      CHARACTER(len=256), PARAMETER :: exe = exe_CosmicEmu
       REAL, PARAMETER :: eps_h = eps_h_CosmicEmu
       REAL, PARAMETER :: kmin_rebin = kmin_rebin_CosmicEmu
       REAL, PARAMETER :: kmax_rebin = kmax_rebin_CosmicEmu   
       INTEGER, PARAMETER :: nk_rebin = nk_rebin_CosmicEmu
 
       ! Remove previous parameter and power file
-      CALL EXECUTE_COMMAND_LINE('rm '//trim(params))
-      CALL EXECUTE_COMMAND_LINE('rm '//trim(output))
+      CALL EXECUTE_COMMAND_LINE('rm -rf '//trim(params))
+      CALL EXECUTE_COMMAND_LINE('rm -rf '//trim(output))
 
       ! Write a new parameter file
       OPEN (7, file=params)
       WRITE (7, fmt='(A20,7F10.5)') trim(output), cosm%om_m*cosm%h**2, cosm%om_b*cosm%h**2, cosm%ns, cosm%sig8, cosm%w, z
       CLOSE (7)
 
-      ! Run emu
-      CALL EXECUTE_COMMAND_LINE(trim(exe)//' '//trim(params))! > /dev/null')
+      IF (present_and_correct(verbose)) THEN
+         CALL EXECUTE_COMMAND_LINE(trim(exe)//' '//trim(params))
+      ELSE
+         CALL EXECUTE_COMMAND_LINE(trim(exe)//' '//trim(params)//' > /dev/null')
+      END IF
 
       ! Get length of emu file
       n = file_length(output)
@@ -157,7 +171,7 @@ CONTAINS
       ! Read in data file
       OPEN (7, file=output)
       DO i = 1-nh, n
-         IF (i == h_li-nh) THEN
+         IF (i == hl-nh) THEN
             READ (7, *) crap, crap, crap, crap, crap, crap, crap, crap, h
             IF (present_and_correct(verbose)) THEN
                WRITE (*, *) 'GET_COSMICEMU_POWER_Z: CMB derived h:', h
@@ -203,27 +217,30 @@ CONTAINS
       LOGICAL, OPTIONAL, INTENT(IN) :: verbose
       INTEGER :: i
       INTEGER, PARAMETER :: nh = 5   ! Length of header
-      CHARACTER(len=256), PARAMETER :: params = 'emu_params.txt'
-      CHARACTER(len=256), PARAMETER :: output = 'emu_power.dat'
-      CHARACTER(len=256), PARAMETER :: exe = '/Users/Mead/Physics/FrankenEmu/emu.exe'
+      CHARACTER(len=256), PARAMETER :: params = params_FrankenEmu
+      CHARACTER(len=256), PARAMETER :: output = output_FrankenEmu
+      CHARACTER(len=256), PARAMETER :: exe = exe_FrankenEmu
       REAL, PARAMETER :: kmin_rebin = kmin_rebin_FrankenEmu
       REAL, PARAMETER :: kmax_rebin = kmax_rebin_FrankenEmu
       INTEGER, PARAMETER :: nk_rebin = nk_rebin_FrankenEmu
 
       ! Remove previous parameter and power file
-      CALL EXECUTE_COMMAND_LINE('rm '//trim(params))
-      CALL EXECUTE_COMMAND_LINE('rm '//trim(output))
+      CALL EXECUTE_COMMAND_LINE('rm -rf '//trim(params))
+      CALL EXECUTE_COMMAND_LINE('rm -rf '//trim(output))
 
       ! Write a new parameter file
       OPEN (7, file=params)
       WRITE (7, fmt='(A20,7F10.5)') trim(output), cosm%om_m*cosm%h**2, cosm%om_b*cosm%h**2, cosm%ns, -cosm%w, cosm%sig8, cosm%h, z
       CLOSE (7)
 
-      ! Run emu
-      CALL EXECUTE_COMMAND_LINE(trim(exe)//' '//trim(params))! > /dev/null')
+      IF (present_and_correct(verbose)) THEN
+         CALL EXECUTE_COMMAND_LINE(trim(exe)//' '//trim(params))
+      ELSE
+         CALL EXECUTE_COMMAND_LINE(trim(exe)//' '//trim(params)//' > /dev/null')
+      END IF
 
       ! Get length of emu file
-      n = file_length(output, verbose=.FALSE.)
+      n = file_length(output)
       n = n-nh
       IF (ALLOCATED(k)) DEALLOCATE (k)
       IF (ALLOCATED(P)) DEALLOCATE (P)
@@ -276,24 +293,30 @@ CONTAINS
       TYPE(cosmology), INTENT(IN) :: cosm
       LOGICAL, OPTIONAL, INTENT(IN) :: rebin
       LOGICAL, OPTIONAL, INTENT(IN) :: verbose
-      CHARACTER(len=256) :: output
       INTEGER :: i
       REAL, PARAMETER :: kmin_rebin = kmin_rebin_MiraTitan
       REAL, PARAMETER :: kmax_rebin = kmax_rebin_MiraTitan
       INTEGER, PARAMETER :: nk_rebin = nk_rebin_MiraTitan
+      CHARACTER(len=256), PARAMETER :: params = params_MiraTitan
+      CHARACTER(len=256), PARAMETER :: output = output_MiraTitan
+      CHARACTER(len=256), PARAMETER :: exe = exe_MiraTitan
 
-      CALL EXECUTE_COMMAND_LINE('rm xstar.dat')
-      CALL EXECUTE_COMMAND_LINE('rm EMU0.txt')
+      ! Remove previous parameter and power file
+      CALL EXECUTE_COMMAND_LINE('rm -rf '//trim(params))
+      CALL EXECUTE_COMMAND_LINE('rm -rf '//trim(output))
 
-      OPEN (7, file='xstar.dat')
+      OPEN (7, file=params)
       WRITE (7, *) (cosm%Om_m*cosm%h**2), (cosm%Om_b*cosm%h**2), cosm%sig8, &
          cosm%h, cosm%ns, cosm%w, cosm%wa, (cosm%om_nu*cosm%h**2), z
       CLOSE (7)
 
-      CALL EXECUTE_COMMAND_LINE('/Users/Mead/Physics/MiraTitan/P_tot/emu.exe')
-      output = 'EMU0.txt'
+      IF (present_and_correct(verbose)) THEN
+         CALL EXECUTE_COMMAND_LINE(trim(exe)//' '//trim(params))
+      ELSE
+         CALL EXECUTE_COMMAND_LINE(trim(exe)//' '//trim(params)//' > /dev/null')
+      END IF
 
-      n = file_length(output, verbose=.FALSE.)
+      n = file_length(output)
       IF (ALLOCATED(k)) DEALLOCATE (k)
       IF (ALLOCATED(P)) DEALLOCATE (P)
       ALLOCATE (k(n), P(n))
@@ -311,7 +334,6 @@ CONTAINS
       CLOSE (7)
 
       ! Convert P(k) to Delta^2(k)
-      !P = P*(k**3)*4.*pi/(2.*pi)**3
       P = Delta_Pk(P, k)
 
       ! Convert k to k/h
